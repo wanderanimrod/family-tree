a3cee0dfa22e507125e312f525a101ec3c6dccbc
wanderanimrod <wanderanimrod@gmail.com>
2014-08-05 23:26:42 +0300

    Adds another static directory to the app for the statics we'll create

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from api.routes import setup_routes
+from backend.app import config
 
 app = Flask(__name__, static_folder='bower_components')
 
@@ -9,6 +10,11 @@ def hello():
     return render_template("index.html")
 
 setup_routes(app)
+
+
+@app.route("/<path:filename>")
+def custom_static(filename):
+    return send_from_directory(app.root_path + "/static/", filename)
 
 if __name__ == "__main__":
     app.run()
