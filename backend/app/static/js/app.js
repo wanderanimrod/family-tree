a14b97d27948bfc986e2c334616bff40d1ae6f4a
wanderanimrod <wanderanimrod@gmail.com>
2014-08-09 17:13:49 +0300

    Adds empty state look

--- backend/app/static/js/app.js
+++ backend/app/static/js/app.js
@@ -1,1 +1,24 @@
-console.log("Custom JS loaded")
+var canvas = d3.select("#canvas");
+var canvasWidth = canvas.style("width").replace("px", "")
+var canvasHeight = canvas.style("height").replace("px", "")
+
+var svg = canvas.append("svg")
+    .attr("width", canvasWidth)
+    .attr("height", canvasHeight)
+
+var personGroup = svg.append("svg")
+    .attr("x", 100)
+    .attr("y", 100)
+
+var g = personGroup.append("g")
+
+var center = {x: canvasWidth/2, y: canvasHeight/2}
+
+var text = g.append('text')
+    .attr("x", center.x)
+    .attr("y", center.y)
+    .attr("text-anchor", "middle")
+    .attr('font-family', 'FontAwesome')
+    .attr('font-size', 80)
+    .attr("fill", "#1D8C5E")
+    .text('\uf055')
