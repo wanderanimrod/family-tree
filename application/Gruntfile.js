61528f08b827d305a67ccdfc61ad9be174de96d4
wanderanimrod <wanderanimrod@gmail.com>
2014-08-10 22:15:09 +0300

    Watching the right js files for upload

--- application/Gruntfile.js
+++ application/Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
 
         watch: {
             browserify: {
-                files: ['js/**/*.js'],
+                files: ['client/js/**/*.js'],
                 tasks: ['browserify']
             }
         }
