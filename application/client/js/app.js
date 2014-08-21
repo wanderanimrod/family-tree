a1c0e14923ca76a2a138b2cbface67da7c30ecdd
wanderanimrod <wanderanimrod@gmail.com>
2014-08-21 21:07:42 +0300

    Terminating JS statements for compartibility

--- application/client/js/app.js
+++ application/client/js/app.js
@@ -1,8 +1,8 @@
-var Promise = require('bluebird')
-var get = Promise.promisify(require('superagent').get)
-var blankState = require('./blank-state.js')
+var Promise = require('bluebird');
+var get = Promise.promisify(require('superagent').get);
+var blankState = require('./blank-state.js');
 
 get('/people/').then(function(response) {
-    var people = response.body.people
-    if(!people.length) blankState.draw()
-})
+    var people = response.body.people;
+    if(!people.length) blankState.draw();
+});
--- application/client/js/blank-state.js
+++ application/client/js/blank-state.js
@@ -1,15 +1,15 @@
 function setupBlankState() {
-    var d3 = require('d3-browserify')
+    var d3 = require('d3-browserify');
 
     var canvas = d3.select("#canvas");
-    var canvasWidth = canvas.style("width").replace("px", "")
-    var canvasHeight = canvas.style("height").replace("px", "")
+    var canvasWidth = canvas.style("width").replace("px", "");
+    var canvasHeight = canvas.style("height").replace("px", "");
 
     var svg = canvas.append("svg")
         .attr("width", canvasWidth)
-        .attr("height", canvasHeight)
+        .attr("height", canvasHeight);
 
-    var center = {x: canvasWidth / 2, y: canvasHeight / 2}
+    var center = {x: canvasWidth / 2, y: canvasHeight / 2};
 
     var text = svg.append('text')
         .attr("x", center.x)
@@ -18,10 +18,10 @@ function setupBlankState() {
         .attr('font-family', 'FontAwesome')
         .attr('font-size', 80)
         .attr("fill", "#1D8C5E")
-        .text('\uf055')
+        .text('\uf055');
 }
 
 
 module.exports = {
     draw: setupBlankState
-}
+};
