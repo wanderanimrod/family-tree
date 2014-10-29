4300949864bb77a0a9d7b7c8fdc8a1ce262261d8
wanderanimrod <wanderanimrod@gmail.com>
2014-10-29 21:45:11 +0300

    Extracts new component instantiation into vue-extensions

--- application/client/components/blank-state/blank-state.js
+++ application/client/components/blank-state/blank-state.js
@@ -1,20 +1,4 @@
-var vue = require('vue');
-var superagent = require('superagent');
-
-var fileFromUrl = function(url, el) {
-    superagent.get(url, function(response) {
-        el.innerHTML = response.text;
-    });
-};
-
-var newComponent = function(cName, opts) {
-    var oldCreated = opts.created;
-    opts.created = function() {
-        fileFromUrl(this.$options.templateFromFile, this.$el);
-        oldCreated && oldCreated.call(this);
-    };
-    return vue.component(cName, opts);
-};
+newComponent = require('../common/vue-extensions').newComponent;
 
 newComponent('blank-state', {
     templateFromFile: "client/components/blank-state/blank-state.html",
--- application/client/components/common/vue-extensions.js
+++ application/client/components/common/vue-extensions.js
@@ -1,5 +1,19 @@
-var fileFromUrl = function(url) {
+var Vue = require('vue');
+var superagent = require('superagent');
+
+var insertHtml = function(url, el) {
     superagent.get(url, function(response) {
-        return response.text;
+        el.innerHTML = response.text;
     });
+};
+
+module.exports = {
+    newComponent: function(cName, opts) {
+        var oldCreated = opts.created;
+        opts.created = function() {
+            insertHtml(this.$options.templateFromFile, this.$el);
+            oldCreated && oldCreated.call(this);
+        };
+        return Vue.component(cName, opts);
+    }
 };
