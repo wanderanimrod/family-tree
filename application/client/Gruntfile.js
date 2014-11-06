f6a847f4b0c642af7cf17461b0e8cafb8d2e3f6c
wanderanimrod <wanderanimrod@gmail.com>
2014-11-06 14:47:03 +0300

    Adds build and default grunt tasks

--- application/client/Gruntfile.js
+++ application/client/Gruntfile.js
@@ -20,4 +20,13 @@ module.exports = function(grunt) {
             }
         }
     });
+
+    grunt.registerTask('build', [
+        'browserify'
+    ]);
+
+    grunt.registerTask('default', [
+        'browserify',
+        'karma'
+    ]);
 };
